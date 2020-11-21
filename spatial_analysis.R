31e96ae9fe0be82b63569220ddcc567d5c7d6137
Kwame Awuah <32483637+tkawuah@users.noreply.github.com>
2020-11-21 03:55:13 +0100

    Update spatial_analysis.R

--- spatial_analysis.R
+++ spatial_analysis.R
@@ -20,26 +20,25 @@ spat_met_func <- function(shp_extent, map){
 
 #import data
 
-d <- readOGR(dsn = 'D:/studies/phd/MachineLearningTutorialPackage/results_maps_and_probs/satara/spatial_analysis', 
+d <- readOGR(dsn = '*/spatial_analysis', 
              layer = 'distance_to_water_3')
 
-svm_map <- raster('D:/studies/phd/MachineLearningTutorialPackage/results_maps_and_probs/satara/
-                  thresholded bin maps/SVM_gl_binMap0.25.tif')
+svm_map <- raster('*/SVM_gl_binMap0.25.tif')
 
 
 
 #... patch metrics for the Sabie and Satara landscapes as a whole
 
-sabie_extent <- readOGR(dsn = 'D:/studies/phd/WV3_Data_JULY2019/010039360030_01/010039360030_01_P001_MUL', 
+sabie_extent <- readOGR(dsn = '*/010039360030_01_P001_MUL', 
                         layer = 'clip_extent')
-sabie_map <- raster('D:/studies/phd/MachineLearningTutorialPackage/results_maps_and_probs/sabi/thresholded bin maps/MLP_gl_binMap0.35.tif')
+sabie_map <- raster('*/MLP_gl_binMap0.35.tif')
 
 sabie_metrics <- spat_met_func(sabie_extent,sabie_map)
 sabie_metrics
 
-satara_extent <- readOGR(dsn = 'D:/studies/phd/WV3_Data_JULY2019/010039360040_01/010039360040_01_P001_MUL', 
+satara_extent <- readOGR(dsn = '*/010039360040_01_P001_MUL', 
                          layer = 'clip_extent')
-satara_map <- raster('D:/studies/phd/MachineLearningTutorialPackage/results_maps_and_probs/satara/thresholded bin maps/SVM_gl_binMap0.25.tif')
+satara_map <- raster('*/SVM_gl_binMap0.25.tif')
 
 satara_metrics <- spat_met_func(satara_extent,satara_map)
 satara_metrics
